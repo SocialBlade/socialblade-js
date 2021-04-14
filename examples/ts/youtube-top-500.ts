261ee3371bc6f2cafd617d57b30135803f2e614a
benicoalvin <81112356+benicoalvin@users.noreply.github.com>
2021-04-15 03:58:05 +0800

    Update youtube-top-500.ts

--- examples/ts/youtube-top-500.ts
+++ examples/ts/youtube-top-500.ts
@@ -1,12 +1,16 @@
-import SocialBlade, { YouTubeTop } from '../../lib'; // from "socialblade"
-import 'isomorphic-unfetch';
+import SocialBlade, { YouTubeUser } from 'socialblade';
 
-const client = new SocialBlade(process.env.SOCIALBLADE_CLIENT_ID!, process.env.SOCIALBLADE_ACCESS_TOKEN!);
+const client: SocialBlade = new SocialBlade(SOCIALBLADE_CLIENT_ID, SOCIALBLADE_ACCESS_TOKEN);
 
-// Costs 10 credits per page / i.e. per 100
-const top = async (total: number): Promise<string[]> => {
-  const pages: number = Math.ceil(total / 100);
-  const results: YouTubeTop[][] = [];
+// Get a YouTube User
+const sbUser: YouTubeUser = await client.youtube.user('socialblade');
+
+const SocialBlade = require("socialblade");
+
+const client = new SocialBlade(SOCIALBLADE_CLIENT_ID, SOCIALBLADE_ACCESS_TOKEN);
+
+// Get a YouTube User
+client.youtube.user('socialblade').then(console.log);
 
   for (let index = 0; index < pages; index++) {
     results.push(await client.youtube.top('subscribers', index + 1));
